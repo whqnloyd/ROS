f44846976efe8d5db10bd208ce346e4f8d72aa10
whqnloyd <yangluodai@outlook.com>
2019-02-22 19:07:42 +0000

    renew

---
+++ object_control/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/husarion/ROS/object_control/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/husarion/ros-workspace/devel;/opt/ros/kinetic".split(';') if "/home/husarion/ros-workspace/devel;/opt/ros/kinetic" != "" else []
