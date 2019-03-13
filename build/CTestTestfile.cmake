052ed38501af7988c235b967cf12f01a253d8d72
whqnloyd <yangluodai@outlook.com>
2019-03-13 21:48:07 +0000

    renew

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yang/ROS/src
-# Build directory: /home/yang/ROS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(object_control/src/stop_sign)
--- build/object_control/src/stop_sign/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yang/ROS/src/object_control/src/stop_sign
-# Build directory: /home/yang/ROS/build/object_control/src/stop_sign
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
