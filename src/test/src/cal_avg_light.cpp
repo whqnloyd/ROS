4ad2bac509df6e8f582097a8cddf9e5e1eebd7ff
whqnloyd <yangluodai@outlook.com>
2019-03-14 18:12:15 +0000

    renew

--- src/test/src/cal_avg_light.cpp
+++ src/test/src/cal_avg_light.cpp
@@ -37,7 +37,7 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "example_node");
 	ros::NodeHandle n("~");
 	ros::Subscriber sub = n.subscribe("/camera/rgb/image_raw", 10, imageCallback);
-	n.param<bool>("print_lights", print_b, false);
+	n.param<bool>("print_lights", print_b, true);
 	light_pub = n.advertise<std_msgs::UInt8>("lights", 1);
 	ros::ServiceClient client=n.serviceClient<std_srvs::Empty>("/image_saver/save");
 	std_srvs::Empty srv;
